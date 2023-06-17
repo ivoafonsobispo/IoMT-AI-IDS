ee16614d0cbaa601ce9cd60f2c300b21c1405f73
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2023-06-17 14:06:04 +0100

    Update clean_csv.py

--- Scripts/IP-Based Flows Dataset/clean_csv.py
+++ Scripts/IP-Based Flows Dataset/clean_csv.py
@@ -5,11 +5,10 @@ import time
 import pandas as pd
 
 if len(sys.argv) != 2:
-    print("Usage: python3 count_lines.py <folder_path>")
+    print("Usage: python3 clean_csv.py <folder_path>")
     sys.exit(1)
 
 folder_path = sys.argv[1]
-
 
 def print_with_timestamp(message):
     """
@@ -22,31 +21,29 @@ def print_with_timestamp(message):
 def clean_csv(folder_path):
     """
     Clean and filter the contents of CSV files in each folder.
-
-    Args:
-        folder_path (str): Path to the folder containing the CSV files.
-
-    Returns:
-        None
     """
     print_with_timestamp("Script started")
     print_with_timestamp(f"Cleaning logs in folder: {folder_path}")
 
     start_time = time.time()
-    # Get the total number of folders
-    total_folders = len(os.listdir(folder_path))
-    current_folder = 0
+    total_folders = 0
+    processed_folders = 0
 
     # Iterate over folders in the specified folder path
     for folder in os.listdir(folder_path):
         folder = os.path.join(folder_path, folder)
 
-        combined_file_path = None
+        if not os.path.isdir(folder):
+            continue
+
+        total_folders += 1
 
         # Find the _combined.csv file
+        combined_file_path = None
         for file in os.listdir(folder):
             if file.endswith("_combined.csv"):
                 combined_file_path = os.path.join(folder, file)
+                break
 
         # Clean and filter the contents of the CSV file
         if combined_file_path:
@@ -54,25 +51,22 @@ def clean_csv(folder_path):
             traffic = os.path.basename(folder)
 
             # Read CSV file
-            df = pd.read_csv(combined_file_path, delimiter=',')
+            df = pd.read_csv(combined_file_path)
 
-            if traffic == 'normal' or traffic == 'camoverflow':
-                # Filter rows based on condition
+            # Filter rows based on condition
+            if traffic in ['normal', 'camoverflow']:
                 df = df[~df['id.orig_h'].isin(['10.10.10.252', '10.10.10.253'])]
             else:
-                # Filter rows based on condition
                 df = df[df['id.orig_h'].isin(['10.10.10.252', '10.10.10.253'])]
 
             # Write the cleaned contents back to the CSV file
             df.to_csv(combined_file_path, index=False)
 
-            # Update progress
-            current_folder += 1
-            print_with_timestamp(f"Progress: {current_folder}/{total_folders} folders processed")
+            processed_folders += 1
+            print_with_timestamp(f"Progress: {processed_folders}/{total_folders} folders processed")
 
     end_time = time.time()
     execution_time = end_time - start_time
     print_with_timestamp(f"Script completed in {execution_time:.2f} seconds")
-
 
 clean_csv(folder_path)
